5281104435b7e59063a3f0f4fb71464d234ac44f
truongkawaii <anhviphg02@gmail.com>
2021-04-03 14:26:49 +0700

    v

---
+++ server/services/render.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+ 
+exports.homeRoutes = (req, res) => {
+  // Make a get request to /api/users
+  axios
+    .get("http://localhost:3000/api/users")
+    .then(function (response) {
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+// Get admin  
+exports.getAdmin = (req,res)=>{
+  // axios
+  // .get("http://localhost:3000/api/users")
+  // .then(function (response) {
+  //   res.render("index", { users: response.data });
+  // })
+  // .catch((err) => {
+  //   res.send(err);
+  // });
+
+  // res.render("./admin/index", { users: response.data });
+  res.render("admin");
+
+}
+exports.add_user = (req, res) => {
+  res.render("add_user");
+};
+
+exports.update_user = (req, res) => {
+  axios
+    .get("http://localhost:3000/api/users", { params: { id: req.query.id } })
+    .then(function (userdata) {
+      res.render("update_user", { user: userdata.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
